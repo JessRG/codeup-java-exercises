8ea60fbc9f242571e99ba3e886627cb19dacd19a
Jesus Rogelio Garza <jesusgarza2511@gmail.com>
2020-08-14 13:47:09 -0500

    Wrapped InputTest main method's definition in a try/catch block and commented out the string overloaded function's

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -5,19 +5,23 @@ public class InputTest {
     public static void main(String[] args) {
 
         Input input = new Input();
-        do {
-            // input.getInt(1, 10);
-            System.out.print("Enter some whole integer number: ");
-            String userInput = input.getString();
-            input.getInt(userInput);
+        try {
+            do {
+                 input.getInt(1, 10);
+//                System.out.print("Enter some whole integer number: ");
+//                String userInput = input.getString();
+//                input.getInt(userInput);
 
-        } while( input.yesNo() );
+            } while( input.yesNo() );
 
-        do {
-//            input.getDouble(1, 10);
-            System.out.print("Enter some double number: ");
-            String userInput = input.getString();
-            input.getDouble(userInput);
-        } while( input.yesNo() );
+            do {
+                input.getDouble(1, 10);
+//                System.out.print("Enter some double number: ");
+//                String userInput = input.getString();
+//                input.getDouble(userInput);
+            } while( input.yesNo() );
+        } catch(Exception ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
