3bdda90a20ad254a1dbc6fd112bebed4da67ff48
Jesus Rogelio Garza <jesusgarza2511@gmail.com>
2020-08-20 12:25:12 -0500

    Completed the first Student class exercise of the Java II: Collections Exercise

---
+++ src/grades/Student.java
@@ -0,0 +1,57 @@
+package grades;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+// #1
+public class Student {
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student(String name) {
+        this.name = name;
+        grades = new ArrayList<>();
+    }
+
+    // returns the student's name
+    public String getName() {
+        return name;
+    }
+
+    // adds the given grade to the grades property
+    public void addGrade(int grade) {
+        grades.add(grade);
+    }
+
+    // returns the average of the students grades
+    public double getGradeAverage() {
+        double sum = 0;
+        for( Integer grade : grades ) {
+            sum += grade;
+        }
+        return sum / grades.size();
+    }
+
+    public static void main(String[] args) {
+        // declare Student object(s)
+        Student[] students = {
+                new Student("Andrei"),
+                new Student("Vanessa"),
+                new Student("Tommy")
+        };
+
+        for(Student stud : students) {
+            // generate random grades for students
+            for (int i = 0; i < 5; i++) {
+                stud.addGrade((int) (Math.random() * 101));
+            }
+        }
+
+        for(Student stud : students) {
+            for (Integer grade : stud.grades) {
+                System.out.println(String.format("grade: %d", grade));
+            }
+            System.out.println(String.format("Average: %f", stud.getGradeAverage()));
+        }
+    }
+}
